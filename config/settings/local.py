2839c4b753a451dea1baf9bc2787824b1820c665
Takaaki_Suzuki <takaaki_suzuki@en-japan.com>
2022-07-07 17:48:59 +0900

    fix:修正漏れ適用

--- config/settings/local.py
+++ config/settings/local.py
@@ -70,8 +70,8 @@ LOGGING = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
