ecaeef757e02197dc0ecd37f543d8092215947ef
Takaaki Suzuki <suzuki.t@sd-craft.com>
2022-08-05 00:13:25 +0900

    テストモックファイルを追加

--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,8 @@
-# Create your tests here.
+from rest_framework.test import APITestCase
+
+
+class TestCheckMain(APITestCase):
+    def test_main(self):
+        expected = True
+        result = True
+        self.assertEqual(result, expected, "テスト動作確認")
